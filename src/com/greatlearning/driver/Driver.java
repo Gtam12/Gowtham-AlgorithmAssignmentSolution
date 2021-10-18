383960196974d0e75555edbaccc1257405c80608
Gowtham <gtamsde@gmail.com>
2021-10-18 17:51:29 +0530

    driver class udpates

--- src/com/greatlearning/driver/Driver.java
+++ src/com/greatlearning/driver/Driver.java
@@ -10,6 +10,22 @@ public class Driver {
 	static Stocks stocks = new Stocks();
 	static StockerService stockerService = new StockerService();
 
+	static int displayMenu() {
+		System.out.println("-----------------------------");
+		System.out.println("1. Display the companies stock prices in ascending order");
+		System.out.println("2. Display the companies stock prices in descending order");
+		System.out.println("3. Display the total number of companies for which stock prices rose today");
+		System.out.println("4. Display the total number of companies for which stock prices declined today");
+		System.out.println("5. Search a specific Stock price");
+		System.out.println("Press 0 to exit");
+		System.out.println("-----------------------------");
+
+		int choice = sc.nextInt();
+
+		return choice;
+
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("enter the no of companies");
@@ -24,31 +40,23 @@ public class Driver {
 
 		}
 
-		System.out.println("-----------------------------");
-		System.out.println("1. Display the companies stock prices in ascending order");
-		System.out.println("2. Display the companies stock prices in descending order");
-		System.out.println("3. Display the total number of companies for which stock prices rose today");
-		System.out.println("4. Display the total number of companies for which stock prices declined today");
-		System.out.println("5. Search a specific Stock price");
-		System.out.println("-----------------------------");
+		int choice = displayMenu();
 
-		int choice = sc.nextInt();
+		while (choice != 0) {
+			switch (choice) {
 
-		switch (choice) {
+			case 1:
+				StockerService.stocksInDescendingOrder();
+				System.out.println(stocks.getStockPrice());
+				System.out.println(StockerService.isPriceHigher());
 
-		case 1:
-			StockerService.stocksInDescendingOrder();
-			System.out.println(stocks.getStockPrice());
-			System.out.println(StockerService.isPriceHigher());
-		
-			break;
-		case 2:
-			StockerService.stocksInAscendingOrder();
-			System.out.println(stocks.getStockPrice());
-			System.out.println(StockerService.isPriceHigher());
-			
-			
+				break;
+			case 2:
+				StockerService.stocksInAscendingOrder();
+				System.out.println(stocks.getStockPrice());
+				System.out.println(StockerService.isPriceHigher());
 
+			}
 		}
 
 	}
